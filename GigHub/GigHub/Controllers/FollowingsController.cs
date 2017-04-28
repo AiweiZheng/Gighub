56e4d71a85069a07a516004ce25e8cfc2352c51a
aiwei zheng <zhengjinsu_123@hotmail.com>
2017-04-28 13:48:46 -0400

    Add following

---
+++ GigHub/GigHub/Controllers/FollowingsController.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Web.Http;
+using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class FollowingsController : ApiController
+    {
+        public ApplicationDbContext _context;
+
+        public FollowingsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto followerDto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Followers.Any(
+                                 f => f.FolloweeId == followerDto.FolloweeId
+                                 && f.FollowerId == userId))
+                return BadRequest("Following already exists.");
+
+            var follower = new Following
+            {
+                FolloweeId = followerDto.FolloweeId,
+                FollowerId = userId
+            };
+
+            _context.Followers.Add(follower);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}

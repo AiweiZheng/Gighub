fc3f27ec7c01718eb769a17eabf2031394b66766
Aiwei Zheng <aiweizheng@gmail.com>
2017-06-24 14:01:44 -0400

    re-structure

---
+++ GigHub/GigHub/Controllers/Api/GenresController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using GigHub.Core;
+using GigHub.Core.Models;
+
+namespace GigHub.Controllers.Api
+{
+    public class GenresController : ApiController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GenresController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IHttpActionResult GetGenres(string query)
+        {
+            return Ok(_unitOfWork.Genres.GetGenres(query));
+        }
+    }
+}
